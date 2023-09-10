efd0f8c7ae840d69d1a4d27eecf8459c5c4be436
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-09-10 16:43:17 -0300

    build: spring initializer

---
+++ src/main/java/com/moredevs/psychclinic/PsychClinicApplication.java
@@ -0,0 +1,13 @@
+package com.moredevs.psychclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moredevs/psychclinic/PsychClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moredevs.psychclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
