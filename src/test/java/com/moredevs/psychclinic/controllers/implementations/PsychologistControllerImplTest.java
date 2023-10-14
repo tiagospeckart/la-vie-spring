22bfc7b601fb5145e355dbb93f084cca00b4a98f
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-10-14 19:29:24 -0300

    test: standardizing to JUnit4 - PsychologistControllerImplTest

--- src/test/java/com/moredevs/psychclinic/controllers/implementations/PsychologistControllerImplTest.java
+++ src/test/java/com/moredevs/psychclinic/controllers/implementations/PsychologistControllerImplTest.java
@@ -1,5 +1,6 @@
 package com.moredevs.psychclinic.controllers.implementations;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moredevs.psychclinic.models.dtos.PsychologistCreateDTO;
 import com.moredevs.psychclinic.models.dtos.PsychologistDTO;
@@ -9,16 +10,15 @@ import com.moredevs.psychclinic.services.PsychologistService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,42 +28,44 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import com.fasterxml.jackson.core.type.TypeReference;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
 public class PsychologistControllerImplTest {
 
-    @Autowired
+    @InjectMocks
+    private PsychologistControllerImpl psychologistControllerImpl;
+
+    @Mock
+    private PsychologistService psychologistService;
+
     private MockMvc mockMvc;
 
-    @MockBean
-    private PsychologistService psychologistService;
+    private ObjectMapper objectMapper;
 
     private PsychologistGetDTO samplePsychologistGetDTO;
     private PsychologistCreateDTO samplePsychologistCreateDTO;
     private PsychologistDTO samplePsychologistDTO;
 
-    private ObjectMapper objectMapper;
-
     @Before
     public void setUp() {
         objectMapper = new ObjectMapper();
+        mockMvc = MockMvcBuilders.standaloneSetup(psychologistControllerImpl).build();
         samplePsychologistGetDTO = createSamplePsychologistGetDTO();
         samplePsychologistDTO = createSamplePsychologistDTO();
         samplePsychologistCreateDTO = createSamplePsychologistCreateDTO();
-
         mockServiceBehavior();
     }
 
     private void mockServiceBehavior() {
         when(psychologistService.create(any(PsychologistCreateDTO.class))).thenReturn(samplePsychologistDTO);
         when(psychologistService.findById(1)).thenReturn(samplePsychologistGetDTO);
+        when(psychologistService.update(any(PsychologistDTO.class))).thenReturn(samplePsychologistDTO);
+        doNothing().when(psychologistService).deleteById(1);
+        when(psychologistService.listAll()).thenReturn(Arrays.asList(samplePsychologistGetDTO));
     }
 
     private PsychologistGetDTO createSamplePsychologistGetDTO() {
-        return samplePsychologistGetDTO = PsychologistGetDTO.builder()
+        return PsychologistGetDTO.builder()
                 .id(1)
                 .name("Jane Doe")
                 .email("jane.doe@example.com")
@@ -72,14 +74,14 @@ public class PsychologistControllerImplTest {
                 .licenseNumber("PSY123456")
                 .specializationArea("Clinical")
                 .biography("Dr. Jane Doe has over 10 years of experience in clinical psychology.")
-                .status(Status.ACTIVE) // Assuming Status is an Enum with ACTIVE as one of its constants
-                .sessions(null) // Populate this if needed
+                .status(Status.ACTIVE)
+                .sessions(null)
                 .isDeleted(false)
                 .build();
     }
 
     private PsychologistDTO createSamplePsychologistDTO() {
-        return samplePsychologistDTO = PsychologistDTO.builder()
+        return PsychologistDTO.builder()
                 .id(1)
                 .name("Jane Doe")
                 .email("jane.doe@example.com")
@@ -89,13 +91,13 @@ public class PsychologistControllerImplTest {
                 .specializationArea("Clinical")
                 .biography("Dr. Jane Doe has over 10 years of experience.")
                 .status(Status.ACTIVE)
-                .sessions(null) // Assuming List<SessionDTO> - populate if needed
+                .sessions(null)
                 .isDeleted(false)
                 .build();
     }
 
     private PsychologistCreateDTO createSamplePsychologistCreateDTO() {
-        return samplePsychologistCreateDTO = PsychologistCreateDTO.builder()
+        return PsychologistCreateDTO.builder()
                 .name("Jane Doe")
                 .email("jane.doe@example.com")
                 .password("StrongP@ss123")
@@ -142,61 +144,37 @@ public class PsychologistControllerImplTest {
 
     @Test
     public void testUpdateById() throws Exception {
-        PsychologistDTO updatedPsychologist = PsychologistDTO.builder()
-                .id(1)
-                .name("Updated Name")
-                .email("updated.email@example.com")
-                .password("UpdatedP@ss123")
-                .phone("+1-555-555-5555")
-                .licenseNumber("UpdatedLicenseNumber")
-                .specializationArea("UpdatedSpecialization")
-                .biography("Updated biography.")
-                .status(Status.ACTIVE)
-                .sessions(null)
-                .isDeleted(false)
-                .build();
-
-        when(psychologistService.update(any(PsychologistDTO.class))).thenReturn(updatedPsychologist);
-
         mockMvc.perform(put("/psychologist/1")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(updatedPsychologist)))
+                        .content(objectMapper.writeValueAsString(samplePsychologistDTO)))
                 .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(updatedPsychologist)));
+                .andExpect(content().json(objectMapper.writeValueAsString(samplePsychologistDTO)));
 
         verify(psychologistService, times(1)).update(any(PsychologistDTO.class));
     }
 
-
-
     @Test
     public void testListAll() throws Exception {
-        List<PsychologistGetDTO> allPsychologists = Arrays.asList(
-                samplePsychologistGetDTO
-        );
-
-        when(psychologistService.listAll()).thenReturn(allPsychologists);
-
         MvcResult result = mockMvc.perform(get("/psychologist")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(header().string("X-Total-Count", String.valueOf(allPsychologists.size())))
                 .andReturn();
 
         MockHttpServletResponse response = result.getResponse();
-        List<PsychologistGetDTO> returnedPsychologists = objectMapper.readValue(response.getContentAsString(), new TypeReference<List<PsychologistGetDTO>>() {});
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
 
-        assertThat(returnedPsychologists.size()).isEqualTo(allPsychologists.size());
+        String responseBody = response.getContentAsString();
+        List<PsychologistGetDTO> returnedPsychologists = objectMapper.readValue(responseBody, new TypeReference<List<PsychologistGetDTO>>() {});
+
+        assertThat(returnedPsychologists.size()).isEqualTo(1);
 
         verify(psychologistService, times(1)).listAll();
     }
 
     @Test
     public void testDeleteById() throws Exception {
-        doNothing().when(psychologistService).deleteById(1);
-
         mockMvc.perform(delete("/psychologist/1"))
                 .andExpect(status().isNoContent());
+
+        verify(psychologistService, times(1)).deleteById(1);
     }
 }
-
